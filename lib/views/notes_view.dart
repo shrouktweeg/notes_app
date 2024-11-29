a1b83a1e7e6a1ea538e1eb4ad50452f483a8e713
Shrouk <shrouk.tweeg74@gmail.com>
2024-11-29 15:48:04 +0200

    create Bottom Sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_notes_bottom_sheet.dart';
 import 'package:notes_app/widgets/note_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -7,7 +8,11 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){},
+      floatingActionButton: FloatingActionButton(onPressed: (){
+         showModalBottomSheet(context: context, builder: (context){
+          return const CustomNotesBottomSheet();
+        });
+      },
       child: const Icon(Icons.add),
       ),
       body:const NoteBody() ,
